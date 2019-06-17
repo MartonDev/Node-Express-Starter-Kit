efad7da93de970f87c398034a8b1eb2e72e9333a
Marton <martondeveloper@gmail.com>
2019-06-17 16:51:10 +0200

    Added files

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+
+const port = 8000;
+
+app.use(express.static(__dirname + '/public', { extensions: ['html'] }));
+
+server.listen(port, function() {
+
+  console.log(`Server started on port ${port}`);
+
+});
