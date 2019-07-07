f29d3dbdbd4de1ce0be36e7e89365ac90915ec3d
Marton <martondeveloper@gmail.com>
2019-07-07 23:29:52 +0200

    Updates

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const express = require('express');
+const session = require('express-session');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 const app = express();
 const server = require('http').Server(app);
-
-const port = 8000;
+const config = require('./config.json');
 
 app.use(express.static(__dirname + '/public', { extensions: ['html'] }));
+app.use(session({
 
-server.listen(port, function() {
+	secret: 'RANDOM(10 length)',
+	resave: true,
+	saveUninitialized: true
 
-  console.log(`Server started on port ${port}`);
+}));
+app.use(bodyParser.urlencoded({extended: true, limit: '50mb'}));
+app.use(bodyParser.json({limit: '50mb'}));
+
+const connection = mysql.createConnection({
+
+  host: 'localhost',
+  user: config.mysql_username,
+  password: config.mysql_password,
+  database: config.mysql_db
+
+});
+connection.connect();
+
+require('./res/routes/post.js')(app, connection);
+require('./res/routes/get.js')(app, connection);
+
+server.listen(config.port, function() {
+
+  console.log(`Server started on port ${config.port}`);
 
 });
