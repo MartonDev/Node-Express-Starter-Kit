2ce8ddcef2ee2eebe321a611ff2e17d33c2125d8
Marton Lederer <martondeveloper@gmail.com>
2020-02-09 11:33:29 +0100

    New version - updated, more professional file structure and API handling

--- app.js
+++ app.js
@@ -1,36 +1,32 @@
-const express = require('express');
-const session = require('express-session');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
-const app = express();
-const server = require('http').Server(app);
-const config = require('./config.json');
+//require modules, server and config
+const express = require('express'),
+bodyParser = require('body-parser'),
+app = express(),
+config = require('./config.json');
 
-app.use(express.static(__dirname + '/public', { extensions: ['html'] }));
-app.use(session({
+//using public folder for get requests
+app.use(express.static(__dirname + '/public', {extensions: ['html']}));
 
-	secret: 'RANDOM(10 length)',
-	resave: true,
-	saveUninitialized: true
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-}));
-app.use(bodyParser.urlencoded({extended: true, limit: '50mb'}));
-app.use(bodyParser.json({limit: '50mb'}));
+//API handlers
+app.all('/api/*', function(request, response, next) {
 
-const connection = mysql.createConnection({
+  try {
 
-  host: 'localhost',
-  user: config.mysql_username,
-  password: config.mysql_password,
-  database: config.mysql_db
+    const handler = require(`./app/api/${request.method.toLowerCase()}.js`);
+    handler.handle(request, response, next);
+
+  }catch(exception) {
+
+    response.json({'error': true, 'message': 'Unsupported request type.'});
+
+  }
 
 });
-connection.connect();
 
-require('./res/routes/post.js')(app, connection);
-require('./res/routes/get.js')(app, connection);
-
-server.listen(config.port, function() {
+app.listen(config.port, function() {
 
   console.log(`Server started on port ${config.port}`);
 
